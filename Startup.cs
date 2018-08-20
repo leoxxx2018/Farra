58327aa975ed254ceefdc8b50e2bac8fd28514df
leoxxx2018 <42553308+leoxxx2018@users.noreply.github.com>
2018-08-20 16:19:07 -0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Farra.Startup))]
+namespace Farra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
